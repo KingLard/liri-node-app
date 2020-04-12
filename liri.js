edc9ef7d0367fbaf50d2388e9132a6c8e33ccc00
Brett Landes <brettl0407@gmail.com>
2020-04-12 15:19:49 -0400

    made the api's into functions so the random text could be passed into them

--- liri.js
+++ liri.js
@@ -13,10 +13,62 @@ var fs = require("fs");
 
  var APIchoice = process.argv[2];
  var option = process.argv[3];
- var queryURL = "http://www.omdbapi.com/?i=tt3896198&apikey=cf6fa77d&t=" + option;
+ 
 
 
- //spotify api -----------------------------------------------------------
+ //spotify api function -----------------------------------------------------------
+ function spotifyAPI(option) {
+  var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
+   spotify.search({ type: 'track', query: option, limit: 1 }, function(err, data) {
+     if (err) {
+       return console.log('Error occurred: ' + err);
+     }
+   
+   var artist = data.tracks.items[0].artists[0].name;
+   var songName = data.tracks.items[0].name;
+   var albumName = data.tracks.items[0].album.name;
+   var preview = data.tracks.items[0].preview_url;
+
+   console.log("Artist: " + artist + "\nSong Name: " + songName + "\nAlbum: " + albumName + "\nSong Preview: " + preview)
+
+   });
+}
+
+
+// movie api function ---------------------------------------------------------------
+
+
+function movieAPI(option) {
+  var queryURL = "http://www.omdbapi.com/?i=tt3896198&apikey=cf6fa77d&t=" + option;
+
+axios
+.get(queryURL)
+.then(function(response) {
+
+
+  var title = response.data.Title;
+  var year = response.data.Year;
+  var rating = response.data.imdbRating;
+  var rottenRating = response.data.Ratings[1].Value;
+  var country = response.data.Country;
+  var language = response.data.Language;
+  var plot = response.data.Plot;
+  var actors = response.data.Actors
+
+
+  console.log("Movie Title: " + title + "\nRelease Year: " + year + "\nIMDB Rating: " + rating + "\nRotten Tomatoes Rating: " + rottenRating + "\nCountry: " + country +
+  "\nLanguages: " + language + "\nPlot: " + plot + "\nActors: " + actors);
+});
+}
+
+
+
+
+
+
 
 
 
@@ -34,63 +86,35 @@ fs.readFile("random.txt", "UTF8", function(error, data) {
     var APIchoice = res[0];
     var option = res[1];
 
+    if (APIchoice === "spotify-this-song") {
+      spotifyAPI(option);
+    } 
+    else if (APIchoice === "movie-this") {
+      movieAPI(option);
+    }
 
     
   }
 })
-  //* It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
 
-  // * Edit the text in random.txt to test out the feature for movie-this and concert-this.
 }
 
 
 
 else if (APIchoice === "spotify-this-song") {
 
-    var spotify = new Spotify({
-        id: spotify.credentials.id,
-        secret: spotify.credentials.secret
-      });
-       spotify.search({ type: 'track', query: option, limit: 1 }, function(err, data) {
-         if (err) {
-           return console.log('Error occurred: ' + err);
-         }
-       
-
-      //  console.log("Artist: " + this.artist + "\nSong Name: " + this.songName + "\nAlbum: " + this.album + "\nSong Preview: " + this.preview)
-       var artist = data.tracks.items[0].artists[0].name;
-       var songName = data.tracks.items[0].name;
-       var albumName = data.tracks.items[0].album.name;
-       var preview = data.tracks.items[0].preview_url;
-
-       console.log("Artist: " + artist + "\nSong Name: " + songName + "\nAlbum: " + albumName + "\nSong Preview: " + preview)
-
-       });
+spotifyAPI(option);
+  
  }
-
+    
 
 //movie api ------------------------------------------------------
 
 
  else if (APIchoice === "movie-this") {
-    axios
-  .get(queryURL)
-  .then(function(response) {
 
+   movieAPI(option);
 
-    var title = response.data.Title;
-    var year = response.data.Year;
-    var rating = response.data.imdbRating;
-    var rottenRating = response.data.Ratings[1].Value;
-    var country = response.data.Country;
-    var language = response.data.Language;
-    var plot = response.data.Plot;
-    var actors = response.data.Actors
-
-
-    console.log("Movie Title: " + title + "\nRelease Year: " + year + "\nIMDB Rating: " + rating + "\nRotten Tomatoes Rating: " + rottenRating + "\nCountry: " + country +
-    "\nLanguages: " + language + "\nPlot: " + plot + "\nActors: " + actors);
-  });
  }
 
 
